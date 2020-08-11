994578a02a57a1ad0c54b6107f70a0bed6a28672
Amber Gershman <67204846+AmberGershman@users.noreply.github.com>
2020-08-11 18:17:04 -0400

    Inadvertenly restarted the kernel and had to start over. But added PNG files and cleared up some messiness.

--- WeatherPy/api_keys.py
+++
@@ -1,6 +0,0 @@
-# OpenWeatherMap API Key
-# weather_api_key = "1101194062628219b1a14259733dd563"
-weather_api_key = "d16608e74d732613b8f1f45bf4f10709"
-
-# Google API Key
-g_key = "AIzaSyD1DeCLJ0_3EmjJHfPDzfqLL2N4IuJ3oeY"
